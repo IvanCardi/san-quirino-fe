d92a01e7b63e7c5311dd5191197a7fec18658c37
IvanCardi <ivancardillo1996@gmail.com>
2025-05-23 12:46:03 +0200

    feat: adds splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 // app/page.tsx
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import logo from "../../public/andromeda-logo.png"
 
 export default function Page() {
   const router = useRouter();
@@ -19,20 +21,20 @@ export default function Page() {
           });
           console.log("Service Worker registered successfully");
           if (isMounted) {
-            router.push("/home"); // Redirect after successful registration
+            // router.push("/home"); // Redirect after successful registration
           }
         } catch (error) {
           console.error("Service Worker registration failed:", error);
           // Decide if you still want to redirect or show an error
           if (isMounted) {
             // Potentially still redirect, or handle error
-            router.push("/home");
+            // router.push("/home");
           }
         }
       } else {
         // Fallback if SW or PushManager not supported
         if (isMounted) {
-          router.push("/home");
+          // router.push("/home");
         }
       }
     }
@@ -44,5 +46,10 @@ export default function Page() {
     };
   }, [router]); // router can be a dependency if used, though here it might be stable
 
-  return <>Andromeda</>; // Or a proper loading/splash screen
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <Image src={logo} alt="logo" width={200} height={200}/>
+      <h1 className="text-[36px]/[36px] font-bold text-[#006e91]">ANDROMEDA</h1>
+    </div>
+  ); // Or a proper loading/splash screen
 }
