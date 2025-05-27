38f1f138b289328590bc4322a265504c2af3196b
IvanCardi <ivancardillo1996@gmail.com>
2025-05-27 14:55:26 +0200

    feat: adds reverse of notifications

--- src/app/notification-center/page.tsx
+++ src/app/notification-center/page.tsx
@@ -12,7 +12,7 @@ export default async function NoficationCenterPage() {
         className="bg-[#02476B]"
       />
       <div className="flex-1 overflow-y-auto px-5 py-5 flex flex-col gap-3">
-        {notifications.map((n) => (
+        {notifications.reverse().map((n) => (
           <div key={n.id} className="rounded-[8px] shadow-md px-4 py-3 flex flex-col gap-2">
             <p className="text-[20px]/[20px] font-bold">{n.title}</p>
             <p className="text-[16px]/[16px] font-light">{n.body}</p>
