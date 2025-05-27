bd1d9cd963f0a848e146b9b16ec8bb23e2a7d8a6
IvanCardi <ivancardillo1996@gmail.com>
2025-05-27 16:03:51 +0200

    fix: fixes address lenght in office

--- src/app/offices/[id]/info.tsx
+++ src/app/offices/[id]/info.tsx
@@ -10,7 +10,7 @@ export default function Info({
   return (
     <div className="flex flex-col m-auto w-fit items-center gap-2">
       <h1 className="text-[19px]/[19px] font-bold">{name}</h1>
-      <p className="text-[15px]/[15px] font-extralight">{address}</p>
+      <p className="text-[15px]/[15px] font-extralight w-[70%]">{address}</p>
       {manager ? (
         <p className="text-[15px]/[15px]">Titolare: {manager}</p>
       ) : (
